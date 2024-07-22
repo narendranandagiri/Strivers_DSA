7be31fd2e22e39164d08a0c88222fa61bfcdca59
narendranandagiri <138883107+narendranandagiri@users.noreply.github.com>
2024-07-22 19:21:29 +0530

    Add files via upload

---
+++ pattern looping/alpha q4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int chrt = 65;
+        for(int j = 0; j < n-i; j++){
+            cout << (char)chrt << " ";
+            chrt+=1;
+        }
+        for(int j = i+1; j < n; j++) cout << "  ";
+        cout << endl;
+    }
+    return 0;
+}
