4d66dbc1268986488ba02e603389fcd0978a02e2
narendranandagiri <138883107+narendranandagiri@users.noreply.github.com>
2024-07-31 18:07:06 +0530

    Update long_subarray_sum.cpp

--- arrays/long_subarray_sum.cpp
+++ arrays/long_subarray_sum.cpp
@@ -1,33 +1,5 @@
 //hashing
 
-class Solution
-{
-    public:
-    //Function to sort an array using quick sort algorithm.
-    void quickSort(int arr[], int low, int high)
-    {
-        if(low < high){
-        int p = partition(arr,low,high);
-        quickSort(arr,low,p-1);
-        quickSort(arr,p+1,high);
-        }
-    }
-    
-    public:
-    int partition (int arr[], int low, int high)
-    {
-        int p = arr[low];
-        int i = low;
-        int j = high;
-        while(i < j){
-            while(arr[i] <= p && i<=high-1) i++;
-            while(p < arr[j] && j>=low+1) j--;
-            if(i < j) swap(arr[i],arr[j]);
-        }
-        swap(arr[low],arr[j]); return j;
-    }
-};
-
 
 
 
