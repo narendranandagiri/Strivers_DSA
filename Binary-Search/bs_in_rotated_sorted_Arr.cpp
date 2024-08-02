20443c66312a411d95948643ed62617fa27e365d
narendranandagiri <138883107+narendranandagiri@users.noreply.github.com>
2024-08-02 12:39:39 +0530

    Update bs_in_rotated_sorted_Arr.cpp

--- Binary-Search/bs_in_rotated_sorted_Arr.cpp
+++ Binary-Search/bs_in_rotated_sorted_Arr.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        int l=0,mid,n=a.size();
+        int h=n-1;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(a[mid]==target) return mid;
+            if(a[l]<=a[mid]){
+                if( a[l]<=target && target<=a[mid])
+                h=mid-1;
+                else
+                l=mid+1;
+            }
+            else{
+                if(a[mid]<=target && target<=a[h])
+                l=mid+1;
+                else
+                h=mid-1;
+            }
+        } //return -1;
+        return -1;
+    }
+};
